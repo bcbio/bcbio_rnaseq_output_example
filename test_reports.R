c099476e6505cca88aef9abe8b47b5686c1b491f
Lorena Pantano <lorena.pantano@gmail.com>
2017-09-06 10:21:09 -0400

    add testthat file

---
+++ test_reports.R
@@ -0,0 +1,36 @@
+context("Reports")
+library(rmarkdown)
+
+test_that("qc_and_de", {
+    unlink("report", recursive = TRUE)
+    extraDir <- system.file("extra", package = "bcbioRNASeq")
+    reportDir <- file.path("..", "..", "bcbioRnaseq",
+        "inst", "rmarkdown",
+        "templates", "quality_control",
+        "skeleton")
+    miscDir <- file.path("..",  "..", "bcbioRnaseq", "docs", "downloads")
+    uploadDir <- tools::file_path_as_absolute(file.path("."))
+    print(uploadDir)
+
+    dir.create("report")
+    setwd("report")
+    print(list.files(miscDir, full.names = TRUE))
+    file.copy(list.files(miscDir, full.names = TRUE), ".",
+              overwrite = TRUE, recursive = TRUE)
+    print(file.path(reportDir, "skeleton.Rmd"))
+    file.copy(file.path(reportDir, "skeleton.Rmd"), "qc.Rmd", overwrite = TRUE)
+
+    render("qc.Rmd", params = list(uploadDir = uploadDir,
+                                   interestingGroups = "group"))
+
+    reportDir <- file.path("..",  "..", "bcbioRnaseq",
+                           "inst", "rmarkdown",
+                           "templates", "differential_expression",
+                           "skeleton")
+
+    file.copy(file.path(reportDir, "skeleton.Rmd"), "de.Rmd", overwrite = TRUE)
+    render("de.Rmd", params = list(design = formula(~group),
+                                   contrast = c("group", "ctrl", "ko")))
+    setwd("..")
+
+})
