c47d0b47b4cb7ee5992dabc3dca7c1511c234a84
Lorena Pantano <lorena.pantano@gmail.com>
2017-09-07 15:45:48 -0400

    Add checkpoint for object loaded

--- test_reports.R
+++ test_reports.R
@@ -1,5 +1,6 @@
 context("Reports")
 library(rmarkdown)
+library(bcbioRNASeq)
 
 test_that("qc_and_de", {
     unlink("report", recursive = TRUE)
@@ -10,18 +11,21 @@ test_that("qc_and_de", {
         "skeleton")
     miscDir <- file.path("..",  "..", "bcbioRnaseq", "docs", "downloads")
     uploadDir <- tools::file_path_as_absolute(file.path("."))
+    
     print(uploadDir)
 
     dir.create("report")
     setwd("report")
+    outputDir <- tools::file_path_as_absolute(file.path("."))
     print(list.files(miscDir, full.names = TRUE))
     file.copy(list.files(miscDir, full.names = TRUE), ".",
-              overwrite = TRUE, recursive = TRUE)
+              overwrite = FALSE, recursive = TRUE)
     print(file.path(reportDir, "skeleton.Rmd"))
     file.copy(file.path(reportDir, "skeleton.Rmd"), "qc.Rmd", overwrite = TRUE)
 
     render("qc.Rmd", params = list(uploadDir = uploadDir,
-                                   interestingGroups = "group"))
+                                   interestingGroups = "group",
+                                   outputDir = outputDir))
 
     reportDir <- file.path("..",  "..", "bcbioRnaseq",
                            "inst", "rmarkdown",
@@ -30,7 +34,12 @@ test_that("qc_and_de", {
 
     file.copy(file.path(reportDir, "skeleton.Rmd"), "de.Rmd", overwrite = TRUE)
     render("de.Rmd", params = list(design = formula(~group),
-                                   contrast = c("group", "ctrl", "ko")))
+                                   contrast = c("group", "ctrl", "ko"),
+                                   outputDir = outputDir))
+    
+    load("data/bcb.rda")
+    print(bcb)
+    
     setwd("..")
 
 })
